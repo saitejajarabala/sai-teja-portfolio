acaa3082fe09d15751df6ae9fa7bc58b703daf55
Sai Teja <jsaiteja2001@gmail.com>
2026-05-30 23:46:00 -0400

    font: change

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
         />
       </head>
       <body>{children}</body>
